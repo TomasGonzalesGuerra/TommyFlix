bbd5dae517b62eb64103099bc126ff87e225d5d5
Tomas Gonzales Guerra <tommy95pro@outlook.com>
2026-03-21 18:40:56 -0500

    UI Reording 😁

--- TommyFlix.Web/Models/Movie.cs
+++ TommyFlix.Web/Models/Movie.cs
@@ -6,7 +6,7 @@ public class Movie
     public string Title { get; set; }
     public string Overview { get; set; }
     public string Release_Date { get; set; }
-    public double Vote_Average { get; set; }
     public string Poster_Path { get; set; }
     public string Backdrop_Path { get; set; }
+    public double Vote_Average { get; set; }
 }
--- TommyFlix.Web/Models/TvSeries.cs
+++ TommyFlix.Web/Models/TvSeries.cs
@@ -5,6 +5,7 @@ public class TvSeries
     public int Id { get; set; }
     public string Name { get; set; }
     public string Overview { get; set; }
+    public string Release_Date { get; set; }
     public string Poster_Path { get; set; }
     public string Backdrop_Path { get; set; }
     public double Vote_Average { get; set; }
