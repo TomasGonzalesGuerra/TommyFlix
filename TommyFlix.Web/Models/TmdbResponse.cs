cf50e789b480ec85092416ca5729499ffd0d1f46
pc <pc@ANTAREZ-01>
2026-03-21 12:59:51 -0500

    yeaaa

---
+++ TommyFlix.Web/Models/TmdbResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TommyFlix.Web.Models;
+
+public class TmdbResponse<T>
+{
+    public int Page { get; set; }
+    public List<T> Results { get; set; }
+    public int Total_Results { get; set; }
+    public int Total_Pages { get; set; }
+}
--- TommyFlix.Web/Services/TmdbService.cs
+++ TommyFlix.Web/Services/TmdbService.cs
@@ -8,13 +8,58 @@ public class TmdbService(HttpClient http, IConfiguration config)
     private readonly HttpClient _http = http;
     private readonly IConfiguration _config = config;
 
-    public async Task<MovieResponse> GetPopularActionMovies()
+    public async Task<Movie> GetMovieDetails(int id)
     {
-        var url = $"discover/movie?api_key={_config["TmdbConfig:ApiKey"]}" +
-                  "&sort_by=popularity.desc" +
-                  "&with_genres=28" +
-                  "&page=1";
+        var url = $"movie/{id}?api_key={_config["TmdbConfig:ApiKey"]}&language=es-ES";
+        return await _http.GetFromJsonAsync<Movie>(url);
+    }
 
-        return await _http.GetFromJsonAsync<MovieResponse>(url);
+    public async Task<TmdbResponse<TvSeries>> GetPopularSeries()
+    {
+        var url = $"tv/popular?api_key={_config["TmdbConfig:ApiKey"]}&language=es-ES";
+        return await _http.GetFromJsonAsync<TmdbResponse<TvSeries>>(url);
+    }
+
+    public async Task<TvSeries> GetSerieDetails(int id)
+    {
+        var url = $"tv/{id}?api_key={_config["TmdbConfig:ApiKey"]}&language=es-ES";
+        return await _http.GetFromJsonAsync<TvSeries>(url);
+    }
+
+    public async Task<TmdbResponse<Movie>> GetPopularMovies()
+    {
+        var url = $"movie/popular?api_key={_config["TmdbConfig:ApiKey"]}&language=es-ES";
+        return await _http.GetFromJsonAsync<TmdbResponse<Movie>>(url);
+    }
+
+    // Opcional: por género (acción = 28)
+    public async Task<TmdbResponse<Movie>> GetPopularActionMovies()
+    {
+        var url = $"discover/movie?api_key={_config["TmdbConfig:ApiKey"]}&with_genres=28&language=es-ES";
+        return await _http.GetFromJsonAsync<TmdbResponse<Movie>>(url);
     }
 }
+
+//public Task<TmdbResponse<Movie>> GetPopularMovies()
+//    {
+//        var url = $"movie/popular?api_key={ApiKey}&language=es-ES";
+//        return _http.GetFromJsonAsync<TmdbResponse<Movie>>(url);
+//    }
+
+//    public Task<TmdbResponse<Movie>> GetPopularActionMovies()
+//    {
+//        var url = $"discover/movie?api_key={ApiKey}&with_genres=28&language=es-ES";
+//        return _http.GetFromJsonAsync<TmdbResponse<Movie>>(url);
+//    }
+
+//    public Task<TmdbResponse<TvSeries>> GetPopularSeries()
+//    {
+//        var url = $"tv/popular?api_key={ApiKey}&language=es-ES";
+//        return _http.GetFromJsonAsync<TmdbResponse<TvSeries>>(url);
+//    }
+
+//    public Task<TmdbResponse<TvSeries>> GetOnTheAirSeries()
+//    {
+//        var url = $"tv/on_the_air?api_key={ApiKey}&language=es-ES";
+//        return _http.GetFromJsonAsync<TmdbResponse<TvSeries>>(url);
+//    }
