bd0b5094c321de80468070ac53d41a62d426b2b0
pc <pc@ANTAREZ-01>
2026-04-06 16:14:47 -0500

    Hambutgrdon 😁

---
+++ TommyFlix.Web/wwwroot/js/scrollHelper.js
@@ -0,0 +1,18 @@
+﻿window.scrollHelper = {
+    _dotnet: null,
+    _handler: null,
+
+    init(dotnet) {
+        this._dotnet = dotnet;
+        this._handler = () => {
+            const scrolled = window.scrollY > 60;
+            dotnet.invokeMethodAsync('OnScroll', scrolled);
+        };
+        window.addEventListener('scroll', this._handler);
+    },
+
+    dispose() {
+        if (this._handler)
+            window.removeEventListener('scroll', this._handler);
+    }
+};
