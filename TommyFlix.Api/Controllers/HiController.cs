e678b5f12214fad407b2d36ce125bc8ccd9c3041
Tomas Gonzales Guerra <tommy95pro@outlook.com>
2026-06-19 21:54:33 -0500

    api/Hi

---
+++ TommyFlix.Api/Controllers/HiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TommyFlix.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HiController : ControllerBase
+{
+    // GET: api/Hi
+    [HttpGet]
+    public async Task<ActionResult<string>> GetAdminDashboardAsync()
+    {
+        return Ok($"Hola soy Yop");
+    }
+}
